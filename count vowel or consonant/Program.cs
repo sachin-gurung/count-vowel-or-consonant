f5d15da9e508942bbf40fe447192fec1a89be6a2
Sachin Gurung <saching061@hotmail.com>
2019-12-10 23:00:33 -0800

    Add project files.

---
+++ count vowel or consonant/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace count_vowel_or_consonant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
