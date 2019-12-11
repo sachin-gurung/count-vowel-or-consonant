df7a53a81de5f339bb27148e86fd90d539bb127a
Sachin Gurung <saching061@hotmail.com>
2019-12-10 23:01:14 -0800

    first  commit

--- count vowel or consonant/Program.cs
+++ count vowel or consonant/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace count_vowel_or_consonant
+public class Exercise9
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        string str;
+        int i, len, vowel, cons;
+
+        Console.Write("\n\nCount total number of vowel or consonant :\n");
+        Console.Write("----------------------------------------------\n");
+        Console.Write("Input the string : ");
+        str = Console.ReadLine();
+
+        vowel = 0;
+        cons = 0;
+        len = str.Length;
+
+        for (i = 0; i < len; i++)
         {
+
+            if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+            {
+                vowel++;
+            }
+            else if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+            {
+                cons++;
+            }
         }
+        Console.Write("\nThe total number of vowel in the string is : {0}\n", vowel);
+        Console.Write("The total number of consonant in the string is : {0}\n\n", cons);
     }
 }
+/*
+Count total number of vowel or consonant :
+----------------------------------------------
+Input the string : good morning everyone
+
+The total number of vowel in the string is : 8
+The total number of consonant in the string is : 11
+
+Press any key to continue . . .
+*/
